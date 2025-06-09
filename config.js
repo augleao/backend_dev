ec5c0d958978b0b4e10b9f4b89aba028ee63e991
augleao <augleao@gmail.com>
2025-06-09 15:12:13 -0300

    ok

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-module.exports = {
-  frontendURL: process.env.FRONTEND_URL || 'http://localhost:3000', // URL do frontend
-  port:        process.env.PORT        || 3001, // Porta do backend
-  jwtSecret:   process.env.JWT_SECRET  || 'uma_chave_super_secreta', // Chave JWT
-  dbURL:       process.env.DATABASE_URL, // URL do banco de dados
-};
