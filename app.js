19c73172838c51c62973e3eca3d8ea392678c889
augleao <augleao@gmail.com>
2025-07-07 19:36:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -35,17 +35,11 @@ const adminRoutes = require('./routes/admin');
 const importarAtosRoutes = require('./routes/importarAtos');
 const fechamentosRoutes = require('./routes/fechamentos');
 
-//const atosPraticados = require('./routes/atos_praticados'); // Rota para atos praticados
-//const atosTabela = require('./routes/atos-tabela'); // Rota para atos praticados
 
-const atosPraticadosRouter = require('./routes/atosPraticados');
-app.use('/api/atos-praticados', atosPraticadosRouter);
+const atosPraticadosRoutes = require('./routes/atosPraticados');
+app.use('/api/atos-praticados', atosPraticadosRoutes);
 
-// Usar rotas
-//app.use('/api/atos-tabela', atosTabela);
-//app.use('/api/atos-praticados', atosPraticados); // Rota para atos praticados
 
-//app.use('/api/atos-praticados', require('./routes/atosPraticados'));
 app.use('/api/codigos-gratuitos', require('./routes/codigosGratuitos'));
 
 app.use('/api/atos', atosRoutes);
